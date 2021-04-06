838db3c8a203531b6a9abdc34312046b6d9476e8
Kristian Gunder Kramås <kristiankramas@outlook.com>
2021-04-06 17:44:55 +0200

    Social preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,13 +60,20 @@ export default function Home() {
     },
   ];
 
-  const navbarHeight = 72;
-
   return (
     <div>
       <Head>
         <title>Anders & Gunder</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:url" content="https://www.andersoggunder.no/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Anders & Gunder" />
+        <meta name="twitter:card" content="Anders & Gunder" />
+        <meta
+          property="og:description"
+          content="Lyst til å utvikle noe? Vi fikser!"
+        />
+        <meta property="og:image" content={"/images/social-preview.png"} />
       </Head>
       <div
         className="text-light"
